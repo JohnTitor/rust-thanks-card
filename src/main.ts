80a3cb1e1b146e147e087ebbae11adf1812f26c0
Yuki Okushi <jtitor@2k36.org>
2023-01-15 17:56:02 +0900

    Debug
    Signed-off-by: Yuki Okushi <jtitor@2k36.org>

--- src/main.ts
+++ src/main.ts
@@ -16,11 +16,10 @@ async function run(): Promise<void> {
     }
     const name: string = core.getInput('name')
     const token: string = core.getInput('github_token')
-    core.info(`Check contributions by ${name}...`)
     const data = extractData(list, name)
     if (Object.keys(data).length === 0) {
       core.setFailed(
-        'Failed to extract data, possibly your name is not in the list'
+        `Failed to extract data by ${name}, possibly your name is not in the list`
       )
       return
     }
