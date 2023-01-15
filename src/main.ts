eede3a567fbf3d0b88a83bbf96bdb07dbfecb43f
Yuki Okushi <jtitor@2k36.org>
2023-01-15 09:16:07 +0900

    Rewrite action
    Signed-off-by: Yuki Okushi <jtitor@2k36.org>

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import axios from 'axios'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const res = await axios.get(
+      'https://raw.githubusercontent.com/rust-lang/thanks/gh-pages/rust/all-time/index.html'
+    )
+    core.info(res.data)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
