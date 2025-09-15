6a659462b97dce316fa4da9d8e15bcbd9809a254
Yuki Okushi <huyuumi.dev@gmail.com>
2025-09-15 16:16:36 +0900

    chore: migrate to Biome

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,8 +1,8 @@
-import {extractData, genBadgeURL, genSVGURL} from '../src/main'
-import {expect, test} from 'vitest'
+import { expect, test } from "vitest";
+import { extractData, genBadgeURL, genSVGURL } from "../src/main";
 
-test('extract data from HTML', () => {
-  const testHTML = `
+test("extract data from HTML", () => {
+	const testHTML = `
   <table>
     <tbody>
       <tr>
@@ -12,30 +12,30 @@ test('extract data from HTML', () => {
       </tr>
     </tbody>
   </table>
-  `
-  const data = extractData(testHTML, 'bors')
-  console.log(data)
-  expect(data.get('name')).toBe('bors')
-  expect(data.get('rank')).toBe('1')
-  expect(data.get('contributions')).toBe('53863')
-})
+  `;
+	const data = extractData(testHTML, "bors");
+	console.log(data);
+	expect(data.get("name")).toBe("bors");
+	expect(data.get("rank")).toBe("1");
+	expect(data.get("contributions")).toBe("53863");
+});
 
-test('generate badge URL', () => {
-  const url = genBadgeURL('1', '40782')
-  console.log(url)
-  expect(url).toBe(
-    'https://img.shields.io/badge/Rust%20Contributions-40782%20contibutions,%201st-orange?logo=rust'
-  )
-})
+test("generate badge URL", () => {
+	const url = genBadgeURL("1", "40782");
+	console.log(url);
+	expect(url).toBe(
+		"https://img.shields.io/badge/Rust%20Contributions-40782%20contibutions,%201st-orange?logo=rust",
+	);
+});
 
-test('extract SVG URL', () => {
-  const url = genSVGURL(
-    '1',
-    '40782',
-    'https://avatars.githubusercontent.com/u/3372342?v=4'
-  )
-  console.log(url)
-  expect(url).toBe(
-    'https://cardivo-woad.vercel.app/api?name=Rust%20Contribution%20Stats%0A&description=Contributions%F0%9F%93%9D:%2040782%20Rank%F0%9F%8F%86:%201st&image=https://avatars.githubusercontent.com/u/3372342?v=4&backgroundColor=%23ecf0f1&disableAnimation=true'
-  )
-})
+test("extract SVG URL", () => {
+	const url = genSVGURL(
+		"1",
+		"40782",
+		"https://avatars.githubusercontent.com/u/3372342?v=4",
+	);
+	console.log(url);
+	expect(url).toBe(
+		"https://cardivo-woad.vercel.app/api?name=Rust%20Contribution%20Stats%0A&description=Contributions%F0%9F%93%9D:%2040782%20Rank%F0%9F%8F%86:%201st&image=https://avatars.githubusercontent.com/u/3372342?v=4&backgroundColor=%23ecf0f1&disableAnimation=true",
+	);
+});
